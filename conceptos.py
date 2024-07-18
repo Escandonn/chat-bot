00d964aa293ba4746011c616286716785f5e7017
Alejandro Escadon <alejandroescandonescobar0@gmail.com>
2024-07-18 05:08:47 -0500

    Fixed the Groq API key initialization in conceptos.py.

--- conceptos.py
+++ conceptos.py
@@ -5,7 +5,6 @@ from groq import Groq
 
 os.environ['GROQ_API_KEY'] = "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"
 client = Groq()
-    api_key=os.environ.get("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"),
 )
 
 chat_completion = client.chat.completions.create(
