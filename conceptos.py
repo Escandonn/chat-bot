cb0edb8c90f0b56478720261a5ba5e9fc564b990
Alejandro Escadon <alejandroescandonescobar0@gmail.com>
2024-07-18 05:08:45 -0500

    Fixed environment variable usage for GROQ API key.

--- conceptos.py
+++ conceptos.py
@@ -1,8 +1,10 @@
 import os
+import os.environ
 
 from groq import Groq
 
-client = Groq(
+os.environ['GROQ_API_KEY'] = "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"
+client = Groq()
     api_key=os.environ.get("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"),
 )
 
@@ -14,6 +16,7 @@ chat_completion = client.chat.completions.create(
         }
     ],
     model="llama3-8b-8192",
+    
 )
 
 print(chat_completion.choices[0].message.content)
