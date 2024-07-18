a8d4d1903419668560504cc20ff49e6c78db456d
Alejandro Escadon <alejandroescandonescobar0@gmail.com>
2024-07-18 05:17:50 -0500

    Updated ejemplo-principal-.py to import memorias module and added functionality to add and retrieve memories.

--- ejemplo-principal-.py
+++ ejemplo-principal-.py
@@ -2,6 +2,8 @@ import os
 
 from groq import Groq
 
+from memorias import add_memoria, get_memorias
+
 os.environ['GROQ_API_KEY'] = "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"
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
@@ -20,7 +22,7 @@ chat_completion = client.chat.completions.create(
         }
     ],
     model="llama3-8b-8192",
-    
 )
 
-print(chat_completion.choices[0].message.content)
+add_memoria(chat_completion.choices[0].message.content)
+print(get_memorias())
